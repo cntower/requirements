507cf4623eb6da3e2581bd845fa3cea68ac87af9
cntower <stasbaturin78@gmail.com>
2022-07-12 20:13:30 +0200

    add angular material

--- apps/developing/src/app/app.module.ts
+++ apps/developing/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { DevelopingFeaturePersonificationModule } from '@requirements/developing
 import { HttpClientModule } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
@@ -16,6 +17,7 @@ import { EffectsModule } from '@ngrx/effects';
     HttpClientModule,
     StoreModule.forRoot({}, {}),
     EffectsModule.forRoot([]),
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
